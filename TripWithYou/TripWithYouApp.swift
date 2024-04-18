f54198e312d63595859e1df7ed5f97c24689515e
haveagoodday-github <tbglzt@Gmail.com>
2024-04-18 15:12:10 +0800

    Initial Commit

---
+++ TripWithYou/TripWithYouApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripWithYouApp.swift
+//  TripWithYou
+//
+//  Created by 吕海锋 on 2024/4/18.
+//
+
+import SwiftUI
+
+@main
+struct TripWithYouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
